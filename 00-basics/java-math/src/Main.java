651b197bebb190770711354ffa104c6d70de17e7
Han Truong Van <hantv12a3@gmail.com>
2024-12-07 13:58:03 +0700

    Java Math

---
+++ 00-basics/java-math/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+
+        // Max
+        System.out.println(Math.max(x, y));
+
+        // Min
+        System.out.println(Math.min(x, y));
+
+        // Square root
+        System.out.println(Math.sqrt(x));
+
+        // Absolute
+        System.out.println(Math.abs(-y));
+
+        // Random numbers
+        System.out.println(Math.random());
+
+        // Random number between 0 and 100
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println(randomNum);
+
+        // In java have many methods of math
+    }
+}
