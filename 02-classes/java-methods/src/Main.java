12340218b52ba604f191d246e23d88337bbce687
Han Truong Van <hantv12a3@gmail.com>
2024-12-09 15:24:14 +0700

    Java Methods

---
+++ 02-classes/java-methods/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    // create a method
+    static void myMethod() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+    }
+}
