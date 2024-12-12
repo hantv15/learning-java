2e964aa532c808d5c038b1ae682449bfd28af47b
Han Truong Van <hantv12a3@gmail.com>
2024-12-12 08:30:28 +0700

    Java Encapsulation

---
+++ 02-classes/java-class-methods/src/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public void fullThrottle() {
+        System.out.println("The car is going as fast it can!");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+}
