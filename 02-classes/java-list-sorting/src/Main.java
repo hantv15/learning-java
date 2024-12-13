1c365330c63f1adf5f06175884c241b5cf2ed4ef
Han Truong Van <hantv12a3@gmail.com>
2024-12-13 15:43:31 +0700

    Java List Sorting

---
+++ 02-classes/java-list-sorting/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Sort an Array List
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        Collections.sort(cars);
+        System.out.println(cars);
+        Collections.sort(cars, Collections.reverseOrder());
+        System.out.println(cars);
+    }
+}
